9263734d67cf4d53ffb83e9bd237190223f9b159
aldy ngere <aldyngere20@gmail.com>
2023-04-05 09:28:13 -0700

    menyelesaikan media  query dengan breakpoint 768px

--- javascript/script.js
+++ javascript/script.js
@@ -7,14 +7,16 @@ displayYear.textContent = year;
 // Mobile Button
 const mobileBtn = () => {
   let menu = document.querySelector(".main-nav-links");
+  let btnBox = document.querySelector(".main-nav-btn-box");
   let mobileBtn = document.getElementById("mobile-btn");
   let menuBtn = document.getElementById("menu-btn");
   let closeBtn = document.getElementById("close-btn");
 
   mobileBtn.onclick = () => {
-    menuBtn.classList.toggle("active");
     closeBtn.classList.toggle("active");
     menu.classList.toggle("active");
+    btnBox.classList.toggle("active");
+    menuBtn.classList.toggle("active");
   };
 };
 
