5745c0d8a80ec49e9594d776383570c6ede7a6f1
aldy ngere <aldyngere20@gmail.com>
2023-04-05 07:44:11 -0700

    membuat struktur serta mendesign mobile btn

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,21 @@
-// Display Year 
+// Display Year
 let displayYear = document.getElementById("displayYear");
 const date = new Date();
 let year = date.getFullYear();
 displayYear.textContent = year;
+
+// Mobile Button
+const mobileBtn = () => {
+  let menu = document.querySelector(".main-nav-links");
+  let mobileBtn = document.getElementById("mobile-btn");
+  let menuBtn = document.getElementById("menu-btn");
+  let closeBtn = document.getElementById("close-btn");
+
+  mobileBtn.onclick = () => {
+    menuBtn.classList.toggle("active");
+    closeBtn.classList.toggle("active");
+    menu.classList.toggle("active");
+  };
+};
+
+mobileBtn();
